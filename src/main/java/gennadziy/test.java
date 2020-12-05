884fbefd21796d9f370c4fa789afa4cf9c010adc
geniy@tut.by <Z251284251284z>
2020-12-05 10:02:15 +0100

    I'm lazy

---
+++ src/main/java/gennadziy/test.java
@@ -0,0 +1,24 @@
+package gennadziy;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/*
+@Author Gennadziy GITHUB/gennadziy
+Class name: test
+Date: 2t
+020-10-17
+Time: 10:18
+*/
+public class test {
+    public static void main ( String[] args ) {
+        double d = 45.4654654;
+        System.out.format ("%1000.4f",d);
+
+    }
+}
